19c5b3f289445bc5419cc5c51a16da1a01c2f35e
Swaraj Shinde <officially.swaraj@gmail.com>
2026-07-02 16:24:27 +0530

    Initial commit

---
+++ Codeforces/YouKnowWho/Sheet 1/C.simple_calculator.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long x,y;
+    cin>>x>>y;
+    if((x>=1 && x<=100000) && (y>=1 && y<=100000)){
+        cout<<x<<" + "<<y<<" = "<<x+y<<endl;
+        cout<<x<<" * "<<y<<" = "<<x*y<<endl;
+        cout<<x<<" - "<<y<<" = "<<x-y<<endl;
+    }
+    return 0;
+}
