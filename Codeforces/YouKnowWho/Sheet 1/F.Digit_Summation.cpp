faa8f3697e95acac4f96b9d5f64103cbff1f42ea
Swaraj Shinde <officially.swaraj@gmail.com>
2026-07-05 23:25:33 +0530

    Add solution of problem F

---
+++ Codeforces/YouKnowWho/Sheet 1/F.Digit_Summation.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long num1,num2;
+    int l1,l2;
+    cin>>num1>>num2;
+    l1=num1%10;
+    l2=num2%10;
+    cout<<l1+l2;
+    return 0;
+}
