483828df3f6104ed1324eab7f39f7e280c0a34b0
Swaraj Shinde <officially.swaraj@gmail.com>
2026-07-08 22:07:41 +0530

    Add solution to problem G, direct formula for sum upto n integers

---
+++ Codeforces/YouKnowWho/Sheet 1/G.Summation.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long num;
+    long long sum;
+    int i;
+    cin>>num;
+    sum=(num)*(num+1)/2;
+    cout<<sum;
+    return 0;
+}
