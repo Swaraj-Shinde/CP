0e6eb28a0a7ac858e22dc6d2620c6e8a07dc24cc
Swaraj Shinde <officially.swaraj@gmail.com>
2026-07-26 22:58:08 +0530

    add solution to problem e

---
+++ Codeforces/YouKnowWho/Sheet 1/e.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r;
+    cin>>r;
+    double area = 3.141592653*r*r;
+    cout<<fixed<<setprecision(9)<<area;
+    return 0;
+}
