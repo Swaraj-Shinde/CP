2d9b456648b650a7c98d830b4d3a0361893be3db
Swaraj Shinde <officially.swaraj@gmail.com>
2026-08-19 23:19:02 +0530

    Add solution to x in sheet 1

---
+++ Codeforces/YouKnowWho/Sheet 1/x.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long l1,l2,r1,r2;
+    cin>>l1>>r1>>l2>>r2;
+    if(r1<l2)
+    {
+        cout<<"-1";
+    }
+    else if(r2<l1)
+    {
+        cout<<"-1";
+    }
+    else if(l1<=l2 && l2<=r1 && r1<=r2)
+    {
+        cout<<l2<<" "<<r1;
+    }
+    else if(l2<=l1 && l1<=r2 && r2<=r1)
+    {
+        cout<<l1<<" "<<r2;
+    }
+    else if(l1<=l2 && l2<=r2 && r2<=r1)
+    {
+        cout<<l2<<" "<<r2;
+    }
+    else if(l2<=l1 && l1<=r1 && r1<=r2)
+    {
+        cout<<l1<<" "<<r1;
+    }
+    else if(l1<=r1 && r1==l2 && l2<=r2)
+    {
+        cout<<r1;
+    }
+    else if(l2<=r2 && r2==l1 && l1<=r1)
+    {
+        cout<<r1;
+    }
+    else
+    {
+        cout<<"-1";
+    }
+    return 0;
+}
