dc877eb36f4b90d741f2e6e8bc14ff7c0606f02b
Swaraj Shinde <officially.swaraj@gmail.com>
2026-08-24 23:35:13 +0530

    Add solution to problem d of sheet 2

---
+++ Codeforces/YouKnowWho/Sheet 2/d.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,flag=0;
+    while(flag!=1)
+    {
+        cin>>n;
+        if(n==1999)
+        {
+            cout<<"Correct";
+            return 0;
+        }
+        else
+        {
+            cout<<"Wrong"<<endl;;
+        }
+    }
+    return 0;
+}
