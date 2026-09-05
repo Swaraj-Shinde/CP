3b35ac22f864bb8b920b288dce78ced32d73b996
Swaraj Shinde <officially.swaraj@gmail.com>
2026-09-05 23:59:27 +0530

    Add solution to problem a in sheet 3

---
+++ Codeforces/YouKnowWho/Sheet 3/a.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin>>n;
+    long long arr[n];
+    long long sum = 0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    if(sum<0)
+    {
+        cout<<-sum;
+    }
+    else
+    {
+        cout<<sum;
+    }
+}
