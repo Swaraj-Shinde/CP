1ef9e232b513eb973252fd5c81c6722c2579559c
Swaraj Shinde <officially.swaraj@gmail.com>
2026-09-12 22:55:21 +0530

    Add solution to problem n in sheet 3

---
+++ Codeforces/YouKnowWho/Sheet 3/n.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    string s;
+    cin>>s;
+    for(int i=0;i<a;i++)
+    {
+        if(s[i]>='0' && s[i]<='9')
+        {
+            continue;
+        }
+        else
+        {
+            cout<<"No";
+            return 0;
+        }
+    }
+    if(s[a]!='-')
+    {
+        cout<<"No";
+        return 0;
+    }
+    for(int i=a+1;i<a+b+1;i++)
+    {
+        if(s[i]>='0' && s[i]<='9')
+        {
+            continue;
+        }
+        else
+        {
+            cout<<"No";
+            return 0;
+        }
+    }
+    cout<<"Yes";
+    return 0;
+}
