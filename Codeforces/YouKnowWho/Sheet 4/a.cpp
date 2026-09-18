a0c7493afa773768d9fe138c432bb8dc099a2a09
Swaraj Shinde <officially.swaraj@gmail.com>
2026-09-18 23:52:17 +0530

    Create a folder named Sheet 4 and add a.cpp

---
+++ Codeforces/YouKnowWho/Sheet 4/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s,t;
+    cin>>s>>t;
+    cout<<s.length()<<" "<<t.length()<<endl;
+    cout<<s<<" "<<t;
+    return 0;
+}
